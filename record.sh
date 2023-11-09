cf9828acd029ea7bf1faecd22e1525a9658ffa85
Max Polzin <polzin.max@gmail.com>
2023-11-09 11:41:38 +0100

    Update recorded topics.

--- record.sh
+++ record.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 ros2 bag record --qos-profile-overrides-path ./qos_profile_override.yaml \
-/imu/data  \
-/tendon_length_node_1/tendon_length \
-/tendon_length_node_2/tendon_length \
 /navsatfix \
 /ina220/data \
 /terrain_estimation_node/slope_estimate_mean \
