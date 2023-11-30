bfc2d15500b2cafef57b18a1b69fecb78ce16d1a
Max Polzin <polzin.max@gmail.com>
2023-11-30 15:38:09 +0100

    Record update.

--- record.sh
+++ record.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-ros2 bag record --qos-profile-overrides-path ./qos_profile_override.yaml \
+ros2 bag record \
 /zed/zed_node/imu/data \
 /turn_counter_node/turn_count \
 /livox/lidar \
-/zed/zed_node/left_raw/image_raw_color \
-/zed/zed_node/right_raw/image_raw_color \
-/zed/zed_node/depth/depth_registered 
+/zed/zed_node/left_raw/image_raw_color/compressed \
+/zed/zed_node/right_raw/image_raw_color/compressed \
+/zed/zed_node/depth/depth_registered
 
 
+# /tf \
+# /zed/zed_node/odom \
+# /zed/zed_node/pose
+
+
+# --qos-profile-overrides-path ./qos_profile_override.yaml
--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${UID:=1000} --uid ${GID:=1000} ros
 
-source "/opt/ros/$ROS_DISTRO/setup.bash"
+echo "source /opt/ros/humble/setup.bash" >> /home/ros/.bashrc
 
-exec "$@"
+su - ros
